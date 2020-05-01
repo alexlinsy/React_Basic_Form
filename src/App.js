a66f386f1518ec4b2417a7d272656cb0b1003694
alexlinsy <39345015+alexlinsy@users.noreply.github.com>
2020-05-01 20:45:24 -0300

    Get form property value by ref and event targets

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ export default function App() {
   const usernameInputRef = useRef();
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`You entered ${usernameInputRef.current.value}`);
+    const userName = event.target.elements.userNameInput.value;
+    //const userName = usernameInputRef.current.value;
+    alert(`You entered ${userName}`);
   }
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input ref={usernameInputRef} type="text" />
+        <label htmlFor="userNameInput">Username:</label>
+        <input id="userNameInput" ref={usernameInputRef} type="text" />
         <button type="submit">Submit</button>
       </div>
     </form>
