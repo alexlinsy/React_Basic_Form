a3c55ed70998379582e3acb6ec9bdd1d6c0142de
alexlinsy <39345015+alexlinsy@users.noreply.github.com>
2020-05-03 17:18:00 -0300

    Practice dynamic forms

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React, { useRef } from "react";
+import React, { /*useRef,*/ useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const usernameInputRef = useRef();
+  //const usernameInputRef = useRef();
+  const [username, setUsername] = useState("");
+  const properUserName = username === username.toLocaleLowerCase();
+  const error = properUserName ? null : "Username must be lower case";
+  function handleChange(event) {
+    setUsername(event.target.value);
+  }
   function handleSubmit(event) {
     event.preventDefault();
-    const userName = event.target.elements.userNameInput.value;
+    //const userName = event.target.elements.userNameInput.value;
     //const userName = usernameInputRef.current.value;
-    alert(`You entered ${userName}`);
+    alert(`You entered ${username}`);
   }
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="userNameInput">Username:</label>
-        <input id="userNameInput" ref={usernameInputRef} type="text" />
-        <button type="submit">Submit</button>
+        <input
+          id="userNameInput"
+          //ref={usernameInputRef}
+          type="text"
+          onChange={handleChange}
+        />
+        <div style={{ color: "red" }}>{error}</div>
+        <button disabled={Boolean(error)} type="submit">
+          Submit
+        </button>
       </div>
     </form>
   );
